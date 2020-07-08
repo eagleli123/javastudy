2f69ebd65538cf81b45e8ddbae1f9385ebe302a5
lihui <lihui@itiger.com>
2020-07-08 19:30:44 +0800

    mod:自动生成serialVersionUID

---
+++ src/main/java/com/example/demo/serialize/SerializeObject.java
@@ -0,0 +1,11 @@
+package com.example.demo.serialize;
+
+import java.io.Serializable;
+
+/**
+ * @author: lihui
+ * @date: 2020-07-01
+ */
+public class SerializeObject implements Serializable {
+    private static final long serialVersionUID = 7823244486492729217L;
+}
