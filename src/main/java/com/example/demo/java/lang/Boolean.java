c27349239b678c479c2e75a6c47295678def1f75
lihui <lihui@itiger.com>
2020-09-02 20:23:21 +0800

    mod:boolean test

---
+++ src/main/java/com/example/demo/java/lang/Boolean.java
@@ -0,0 +1,14 @@
+package com.example.demo.java.lang;
+
+import org.apache.commons.lang3.BooleanUtils;
+
+/**
+ * @author: lihui
+ * @date: 2020-08-13
+ */
+public class Boolean {
+    public static void main(String[] args) {
+        System.out.println(BooleanUtils.isTrue(null));
+        System.out.println(BooleanUtils.isFalse(null));
+    }
+}
