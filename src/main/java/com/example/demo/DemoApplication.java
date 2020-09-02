184b2b65f9c9f4fb61da0389df7f73cf9cec3420
lihui <lihui@itiger.com>
2020-09-02 20:26:32 +0800

    mod:DemoApplication

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,13 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@ComponentScan("com.example.demo.spring.application.ioc.scope")
+@EnableAspectJAutoProxy(exposeProxy = true)
+@ComponentScan("com.example.demo.vue")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
 
+        //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoApplication.class);
+        //NormalBean normalBean = context.getBean(NormalBean.class);
+        //normalBean.send();
+    }
 }
